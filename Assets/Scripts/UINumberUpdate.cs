dd59ca8d2402df20a3bebd189949b1ad13c8898c
NilsGarskog <93979268+NilsGarskog@users.noreply.github.com>
2024-04-08 17:03:04 +0200

    UIUpdate

--- Assets/Scripts/UINumberUpdate.cs
+++ Assets/Scripts/UINumberUpdate.cs
@@ -9,14 +9,18 @@ public class UINumberUpdate : MonoBehaviour
     public GameObject player;
     public TextMeshProUGUI bombText;
     public TextMeshProUGUI arrowText;
+    public TextMeshProUGUI EnergyText;
     public Image AliveHeadImage;
     public Image DeadHeadImage;
+    public Slider slider;
 
     // Update is called once per frame
     void Update()
     {
         bombText.text = ("X" + player.GetComponent<BombController>().bombsRemaining.ToString());
         arrowText.text = ("X" + player.GetComponent<BowController>().arrowsRemaining.ToString());
+        EnergyText.text = ("X" + player.GetComponent<EnergyBallController>().energyBallAmount.ToString());
+        slider.value = player.GetComponent<Player>().currentHealth;
 
         if (player.GetComponent<Player>().currentHealth <= 0)
         {
