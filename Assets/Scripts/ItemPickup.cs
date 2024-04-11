2e351ccdf3519ad6e6ed7b5748b56b3e8ccdf339
Samuel Ram <samuel.gh.ram@gmail.com>
2024-04-11 11:21:45 +0200

    ColdShield

--- Assets/Scripts/ItemPickup.cs
+++ Assets/Scripts/ItemPickup.cs
@@ -4,22 +4,20 @@ using UnityEngine;
 
 public class ItemPickup : MonoBehaviour
 {
+    private float baseShieldTime = 7f;
+    private float currentShieldTime = 0f;
     public enum ItemType
     {
         Extrabomb,
         BlastRadius,
         SpeedIncrease,
-        ExtraArrow,
+        ExtraSkull, 
         EnergyBall,
-        ExtraSkull,
+        ExtraArrow,
         ColdShield,
     }
-
-    public ItemType Type;
-    private float baseShieldTime = 7f;
-    private float currentShieldTime = 0f;
     private int coldShieldOrderInLayer = 6;
-
+    public ItemType Type;
     private Coroutine shieldCoroutine;
     private void ActivateShield(GameObject player)
     {
@@ -38,33 +36,40 @@ public class ItemPickup : MonoBehaviour
         player.GetComponent<Mover>().isShielded = false;
         currentShieldTime = 0f;
     }
-
     private void onItemPickup(GameObject player)
     {
         switch (Type)
         {
 
-            case ItemType.Extrabomb:
+             case ItemType.Extrabomb:
                 player.GetComponent<BombController>().AddBomb();
+                Destroy(gameObject);
                 break;
 
             case ItemType.BlastRadius:
                 player.GetComponent<BombController>().explosionRadius += 1;
+                Destroy(gameObject);
                 break;
             case ItemType.SpeedIncrease:
                 player.GetComponent<Mover>().moveSpeed += 1;
+                Destroy(gameObject);
                 break;
             case ItemType.ExtraArrow:
                 player.GetComponent<BowController>().arrowsRemaining += 1;
+                Destroy(gameObject);
                 break;
             case ItemType.EnergyBall:
                 player.GetComponent<EnergyBallController>().AddEnergyBall();
+                Destroy(gameObject);
                 break;
             case ItemType.ExtraSkull:
                 player.GetComponent<SkullController>().AddSkull();
+                Destroy(gameObject);
                 break;
+
+
             case ItemType.ColdShield:
-              if(player.GetComponent<Mover>().isShielded == false)
+                if(player.GetComponent<Mover>().isShielded == false)
                 {
                 ActivateShield(player);
                 StartCoroutine(DestroyAfterDelay(gameObject));
@@ -76,14 +81,11 @@ public class ItemPickup : MonoBehaviour
                     itemRenderer.sortingOrder = coldShieldOrderInLayer;
                 }
                 }
-
-
-
-
+                break;
         }
-        Destroy(gameObject);
+        
     }
-     private void Update()
+    private void Update()
     {
         if(Type == ItemType.ColdShield && transform.parent != null)
         {
@@ -96,7 +98,6 @@ public class ItemPickup : MonoBehaviour
         yield return new WaitForSeconds(currentShieldTime);
         Destroy(obj);
     }
-
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
